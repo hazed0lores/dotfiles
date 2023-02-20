b9632a3f1331a1fc9ce43e4c7ba29c43bd9d9574
thef0xx <others.obtainers@simplelogin.fr>
2023-02-20 17:43:13 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,64 @@
 #!/bin/bash
 
-sudo pacman -S zsh kitty xorg-xrdb bat btop lsd --needed --noconfirm ## Installing required programs ##
-cp -r .config/* $HOME/.config/ ## Copying everything from .config to $HOME/.config ##
-ln -s ~/.config/shell/profile ~/.zprofile ## Creating a symlink for .zprofile (Changes made in .zprofile are applied after logout and logging in again) ##
+# Define constants
+CONFIG_DIR="$HOME/.config"
+ZSH_PLUGINS_DIR="$CONFIG_DIR/zsh"
+PKG_LIST="pkglist.txt"
+AUR_LIST="aur.txt"
 
-###################################### zsh plugins i use #################################################################################
-git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.config/zsh/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.config/zsh/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/zsh/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.config/zsh/powerlevel10k
-##########################################################################################################################################
+# Define functions
+install_required_programs() {
+    sudo pacman -S zsh kitty xorg-xrdb bat btop lsd --needed --noconfirm
+}
 
-#### Randomly insults the users when entering an invalid command. (Works in zsh and bash) Comment out the next two lines if u dont need it 
-git clone https://github.com/hkbakke/bash-insulter.git bash-insulter
-sudo mv bash-insulter/src/bash.command-not-found /etc/
-#############################################################################################################################################
+copy_configuration_files() {
+    cp -r .config/* "$CONFIG_DIR"
+}
 
-chsh -s $(which zsh) ## Changing the default user shell to zsh ##
-sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)" ## Install starship prompt ##
-xrdb merge ~/.config/x11/Xresources ## Xresources ##
-sudo mkdir -p /usr/share/btop/themes;sudo mv catppuccin.theme /usr/share/btop/themes  
-###########################################################################################
+install_zsh_plugins() {
+    git clone https://github.com/zsh-users/zsh-history-substring-search "$ZSH_PLUGINS_DIR/zsh-history-substring-search"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting"
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_PLUGINS_DIR/zsh-autosuggestions"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_PLUGINS_DIR/powerlevel10k"
+}
 
-######### Copies pacman hooks to /etc/pacman.d/hooks/ ###########
-sudo mkdir /etc/pacman.d/hooks/
-sudo cp etc/pacman.d/hooks/* /etc/pacman.d/hooks/
-sed -i "s/admin/$USER/" /etc/pacman.d/hooks/pkglist.hook
-sed -i "s/admin/$USER/" /etc/pacman.d/hooks/aur.hook
-#### Asks the user if they want to install programs in pkglist.txt ####
+install_bash_insulter() {
+    git clone https://github.com/hkbakke/bash-insulter.git bash-insulter
+    sudo mv bash-insulter/src/bash.command-not-found /etc/
+}
 
-echo "Install Programs from pkglist.txt ?"
-read pkglist
-[ $pkglist = "y" ] && sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+change_default_shell() {
+    chsh -s "$(which zsh)"
+}
 
-echo "Install programs from aur.txt ? "
-read aur 
-[ $aur = "y" ] && paru -S --needed - < aur.txt
+install_starship_prompt() {
+    sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+}
+
+merge_xresources() {
+    xrdb merge "$CONFIG_DIR/x11/Xresources"
+}
+
+install_btop_theme() {
+    sudo mkdir -p /usr/share/btop/themes
+    sudo mv catppuccin.theme /usr/share/btop/themes
+}
+
+copy_pacman_hooks() {
+    sudo mkdir -p /etc/pacman.d/hooks
+    sudo cp etc/pacman.d/hooks/* /etc/pacman.d/hooks
+    sed -i "s/admin/$USER/" /etc/pacman.d/hooks/pkglist.hook
+    sed -i "s/admin/$USER/" /etc/pacman.d/hooks/aur.hook
+}
+
+install_pkglist_programs() {
+    echo "Install programs from $PKG_LIST?"
+    read install_pkglist
+    [ "$install_pkglist" = "y" ] && sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort "$PKG_LIST"))
+}
+
+install_aur_programs() {
+    echo "Install programs from $AUR_LIST?"
+    read install_aur
+    [ "$install_aur" = "y" ] && paru -S --needed - < "$AUR_LIST"
+}
