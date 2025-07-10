019a9181a1c60c108162dac1398d37de9766eeb3
Haze <git@hazedolores.org>
2025-07-10 23:54:30 +0530

    make the stow script backup to ~/.config/backup instead of polluting it

--- stow.sh
+++ stow.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+mkdir $HOME/.config/backup
+
 for dir in .config/*; do
   name=$(basename "$dir")
   target="$HOME/.config/$name"
 
   if [ -e "$target" ] && [ ! -L "$target" ]; then
-    echo "Backing up existing $target to $target.backup"
-    mv "$target" "$target.backup"
+    echo "Backing up existing $target to $HOME/.config/backup/$name"
+    mv "$target" "$HOME/.config/backup"
   fi
 done
 
